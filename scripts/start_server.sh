fe8902ee3f661d0603734170fa7f5ae704957426
EC2 Default User <ec2-user@ip-172-31-24-3.ap-southeast-1.compute.internal>
2025-04-27 14:37:38 +0000

    Make error in the line 5

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 sudo systemctl start tomcat.service
 sudo systemctl enable tomcat.service
 sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
+sudo systemctlll enable httpd.service
